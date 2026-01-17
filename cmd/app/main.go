e095461ce5f11ac385bb483f987686864b34580d
Sazikoff <slava.sazikov@yandex.ru>
2026-01-17 21:05:59 +0300

    add new files from hexlet lesson "payroll" & "employees"

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-	"github.com/Sazikoff/hexlet-struct/internal/structure"
 	"fmt"
+	"github.com/Sazikoff/hexlet-struct/internal/employees"
+	"github.com/Sazikoff/hexlet-struct/internal/payroll"
 )
 
 func main() {
-	var parcel1 = structure.Parcel{ID: "BR-7", Destination: "Berlin", Weight: 12, Insured: true}
-	fmt.Println(structure.Describe(parcel1))
+	alex := employees.Employee{
+
+		Name:     "Alex",
+		Position: "Junior",
+		// baseSalary: 100,
+		Skills: []string{"hard worker", "smart", "Honest"},
+	}
+
+	var test bool
+	for {
+		test = alex.SetBaseSalary(100)
+		if test == true {
+			break
+		}
+	}
+	fmt.Println(alex.BaseSalary())
+	dirtyCash := payroll.CalcGross(alex, 50)
+	fmt.Println(dirtyCash)
+	fmt.Println(payroll.CalcNet(dirtyCash, 0.13))
+
 }
